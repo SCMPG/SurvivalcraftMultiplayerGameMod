28449238421d9cebffc476da3bde0935224344ed
fan Bion <1246454212@qq.com>
2022-01-16 23:17:51 +0800

    220116 添加项目文件。

---
+++ BaseFunc/Interface/Class1.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Xml.Linq;
+using Engine;
+using Engine.Graphics;
+using Engine.Input;
+using Game;
+using GameEntitySystem;
+using TemplatesDatabase;
+
+namespace SCMPG
+{
+	public class SCMPGPlayerScreen : PlayerScreen
+	{
+		public override void Leave()
+		{
+			base.Leave();
+			Log.Information("Cool");
+		}
+
+		public override void Update()
+		{
+			base.Update();
+			if (base.Input.Back || base.Input.Cancel || Children.Find<ButtonWidget>("TopBar.Back").IsClicked)
+			{
+				DialogsManager.ShowDialog(null, new MessageDialog("S", "s", "OK", "Cancel", null));
+
+			}
+		}
+	}
+}
---
+++ BaseFunc/Message/Message.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Threading.Tasks;
+using Comms;
+
+namespace SCMPG
+{
+	public abstract class Message
+	{
+		private static Dictionary<int, Type> MessageTypesByMessageId;
+
+		private static Dictionary<Type, int> MessageIdsByMessageTypes;
+
+		static Message()
+		{
+			MessageTypesByMessageId = new Dictionary<int, Type>();
+			MessageIdsByMessageTypes = new Dictionary<Type, int>();
+			TypeInfo[] array = (from t in typeof(Message).GetTypeInfo().Assembly.DefinedTypes
+								where typeof(Message).GetTypeInfo().IsAssignableFrom(t)
+								orderby t.Name
+								select t).ToArray();
+			for (int i = 0; i < array.Length; i++)
+			{
+				MessageTypesByMessageId[i] = array[i].AsType();
+				MessageIdsByMessageTypes[array[i].AsType()] = i;
+			}
+		}
+
+		public static Message Read(byte[] bytes)
+		{
+			BinaryReader binaryReader = new BinaryReader(new MemoryStream(bytes));
+			byte key = binaryReader.ReadByte();
+			if (MessageTypesByMessageId.TryGetValue(key, out var value))
+			{
+				Message obj = (Message)Activator.CreateInstance(value);
+				obj.Read(binaryReader);
+				return obj;
+			}
+			return null;
+		}
+
+		public static byte[] Write(Message message)
+		{
+			BinaryWriter binaryWriter = new BinaryWriter(new MemoryStream());
+			binaryWriter.Write((byte)MessageIdsByMessageTypes[message.GetType()]);
+			message.Write(binaryWriter);
+			return ((MemoryStream)binaryWriter.BaseStream).ToArray();
+		}
+
+		protected abstract void Read(BinaryReader reader);
+
+		protected abstract void Write(BinaryWriter writer);
+
+		/*protected static void WriteAddress(BinaryWriter writer, IPEndPoint address)
+		{
+			SocketAddress socketAddress = address.Serialize();
+			byte[] array = new byte[socketAddress.Size];
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = socketAddress[i];
+			}
+			writer.Write((byte)socketAddress.Family);
+			writer.Write((byte)array.Length);
+			writer.Write(array);
+		}*/
+	}
+}
